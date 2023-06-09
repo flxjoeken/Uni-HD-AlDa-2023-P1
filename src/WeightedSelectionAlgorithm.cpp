d46335dfa73aa6788da9f914123afa1eaf06b300
Felix Joeken <felix@joeken.eu>
2023-06-09 21:02:27 +0200

    inital commit

---
+++ src/WeightedSelectionAlgorithm.cpp
@@ -0,0 +1,9 @@
+#include "WeightedSelectionAlgorithm.h"
+#include <vector>
+
+int WeightedSelectionAlgorithm::weightedSelectionAlgorithm(std::vector<Element> &elements, double q) {
+//TODO: your Algorithm implementation goes here to calculate the qValue:
+
+  int qValue = 0;
+  return qValue;
+}
